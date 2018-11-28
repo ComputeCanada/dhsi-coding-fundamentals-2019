d533f569074b16c62c146e4579a9c1348bd89261
Jessica Otis <jmo4g@virginia.edu>
2018-11-28 15:59:08 -0500

    updating mapping example to read from csv

--- mapFlights.py
+++ mapFlights.py
@@ -1,26 +1,46 @@
 # Google spreadsheet/map of hometowns and travel routes
 
+# import libraries needed for program
+
 import gmplot
+import csv
 
-# import place names, or in this stub, arbitrarily code in lat/long points
+# open csv file of places and read in the latitudes/longitudes; originalDatesReader is a list of lists
 
-latitude = [0, 5, 10]
-longitude = [0, 10, 5]
+placesFile = open("places.csv")
+placesReader = csv.reader(placesFile)
 
-#center map of first lat/long point, zoom level 5
+# create and center map on the University of Victoria, zoom level 3
 
-gmap = gmplot.GoogleMapPlotter(latitude[0],longitude[0], 5) 
+gmap = gmplot.GoogleMapPlotter(48.4634, -123.3117, 3)
 
-# in theory can use scatter to make all the points at once, haven't been able to get this working yet
-# gmap.scatter(latitude, longitude, '#FF66666', edge_width=10)
+# create lists of latitudes and longitudes
+# we are looping through each list in placesReader
 
-# initialize and use a while loop to go through all the points in the lat/long arrays and plot on map
+for location in placesReader:
+	gmap.marker(float(location[0]), float(location[1]))
 
-i = 0
-while i < len(latitude):
-	gmap.marker(latitude[i],longitude[i])
-	i = i+1
+##this was the original code for an arbitrary set of lat/long points
+#latitude = [0, 5, 10]
+#longitude = [0, 10, 5]
+#
+##center map of first lat/long point, zoom level 5
+#
+#gmap = gmplot.GoogleMapPlotter(latitude[0],longitude[0], 5) 
+#
+## in theory can use scatter to make all the points at once, haven't been able to get this working yet
+## gmap.scatter(latitude, longitude, '#FF66666', edge_width=10)
+#
+## initialize and use a while loop to go through all the points in the lat/long arrays and plot on map
+#
+#i = 0
+#while i < len(latitude):
+#	gmap.marker(latitude[i],longitude[i])
+#	i = i+1
 
 # create map, save as map.html in the working directory
 
 gmap.draw('map.html')
+
+# close the files so we don't accidentally corrupt them or crash something
+placesFile.close()
